db88dcdf7cabc94b46aad9b066212b98c07b773f
Friedjoff Trautwein <friedjoff.trautwein@geops.com>
2025-11-17 11:17:14 +0100

    Implement initial i18n config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    defaultLocale: "de",
+    locales: ["de", "fr", "it"],
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
   },
